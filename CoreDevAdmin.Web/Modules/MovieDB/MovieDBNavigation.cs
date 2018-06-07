cbc3322ea80be39efe3a1dc408e1da7f1f9a0017
Robbie Hickey <robbie.hickey@msionline.com>
2018-06-07 13:16:25 -0500

    customizing movie navigation

--- CoreDevAdmin.Web/Modules/MovieDB/MovieDBNavigation.cs
+++ CoreDevAdmin.Web/Modules/MovieDB/MovieDBNavigation.cs
@@ -1,5 +1,5 @@
 ﻿using Serenity.Navigation;
 using MyPages = CoreDevAdmin.MovieDB.Pages;
 
-[assembly: NavigationMenu(7000, "MovieDB", icon: "fa-desktop")]
-[assembly: NavigationLink(int.MaxValue, "MovieDB/Movie", typeof(MyPages.MovieController), icon: "fa-desktop")]
+[assembly: NavigationMenu(7000, "Movie Database", icon: "fa-film")]
+[assembly: NavigationLink(int.MaxValue, "Movie Database/Movies", typeof(MyPages.MovieController), icon: "fa-video-camera")]
